eeff5fdbf73a279343399083be0cdc05d2708e92
Justin <justin@Justin.local>
2017-10-02 20:48:17 +0800

    devise_rolify_cancancan

---
+++ app/models/item.rb
@@ -0,0 +1,3 @@
+class Item < ApplicationRecord
+    resourcify
+end
---
+++ db/migrate/20171002114603_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.string :status
+      t.integer :amount
+      t.integer :price
+      t.string :picture
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
