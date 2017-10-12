a1c1e21cf6f6724e5e346a7a229d5958d774cdda
Justin <justinsax1206@gmail.com>
2017-10-12 23:42:08 +0800

    SaleAblityDone

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,15 @@ class Ability
 
   def initialize(user) #這個 user 其實就是 devise 提供的 current_user
     if user.blank? # not logged in
-      can :create, devise
+      can :create, User
+      basic_read_only
     elsif user.has_role?(:admin) #如果 role 為 admin
       can :manage, :all #可管理所有資源
     elsif user.has_role?(:sale)
       can :manage, Item, user_id: user.id
       can :manage, User, user_id: user.id
     else
-      basic_read_only
+      member_only
     end
   end
 
@@ -18,8 +19,13 @@ class Ability
 
   def basic_read_only
     can :read, Item
+  end
+
+  def member_only
+    can :read, Item
     can :manage, User
   end
+
 end
 
 #:manage: 是指這個 controller 內所有的 action
