9613033d16c54cabbdd03866d04f599b5f7fb03e
wei <acer1456@gmail.com>
2017-11-19 00:57:58 +0800

    add action cable addons to heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.web_socket_server_url = "wss://shushop.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://shushop.herokuapp.com', 'http://shushop.herokuapp.com']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
