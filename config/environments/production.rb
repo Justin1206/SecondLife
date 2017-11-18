334c392006250f52b1978930e64a45580fab9fd3
wei <acer1456@gmail.com>
2017-11-19 01:57:16 +0800

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.web_socket_server_url = "wss://shushop.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://shushop.herokuapp.com', 'http://shushop.herokuapp.com']
+  # config.web_socket_server_url = "wss://shushop.herokuapp.com/cable" 
+  # config.action_cable.allowed_request_origins = ['https://shushop.herokuapp.com', 'http://shushop.herokuapp.com']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://shushop.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://shushop.herokuapp.com.com', /http:\/\/shushop.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
