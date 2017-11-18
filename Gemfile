ee5edde184887c597beb0490fc0c46e9a9e3d252
wei <acer1456@gmail.com>
2017-11-19 01:41:32 +0800

    add mount cable to routes

--- Gemfile
+++ Gemfile
@@ -70,7 +70,6 @@ end
 group :production do
   gem 'pg'
   gem 'rails_12factor'  
-  gem 'redis'
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do  
 
+  mount ActionCable.server => '/cable'  
+
   devise_for :users
   root to: 'items#index'
 
@@ -21,7 +23,6 @@ Rails.application.routes.draw do
 
     resources :messages, only: [:create]
   end
-
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
