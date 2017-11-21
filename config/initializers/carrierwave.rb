742fda816a2d545aed1f6ff06b873b3859bcc117
wei <acer1456@gmail.com>
2017-11-21 16:10:39 +0800

    fix s3 key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',       # required
-      :aws_access_key_id      => 'AKIAJ5BLPQH7G57L72KA',       # your aws access key id
-      :aws_secret_access_key  => '2eKfj8yuERe+usPmyITwnC2o9czU+HX6PsBnykqY',       # your aws secret access key
-      :region                 => 'ap-northeast-1'  # your bucket's region in S3, defaults to 'us-east-1'
+      :aws_access_key_id      => 'AKIAJC6AJGVZ4GKDWCAA',       # your aws access key id
+      :aws_secret_access_key  => 'TTCcHIYIcyGS/k3Uj9SNL3A/U0MsI5BIXc8epeNt',       # your aws secret access key
+      :region                 => 'sa-east-1'  # your bucket's region in S3, defaults to 'us-east-1'
     }
     # your S3 bucket name
     config.fog_directory  = 'shucoupon'
